c4cb01f5a1fe97bd2c10fc39636726d855d1e4fd
jplramos <joaopedrolugarinhoramos@gmail.com>
2024-09-16 16:32:24 -0300

    Coreção

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
   
     if(html.classList.contains("light")) {
-      img.setAttribute("src", "./avatar-light.png")
+      img.setAttribute("src", "./assets/avatar-light.png")
     } else {
-      img.setAttribute("src", "./avatar.png")
+      img.setAttribute("src", "./assets/avatar.png")
     }
 }
